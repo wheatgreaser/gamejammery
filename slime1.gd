c03d11e9ed58cc4582ee572065646cad2d24b65a
wheatgreaser <wheatgreaser3000@gmail.com>
2025-01-22 21:37:24 +0530

    yup

--- slime1.gd
+++ slime1.gd
@@ -6,10 +6,11 @@ const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+var flag = 1
 
 func _physics_process(delta):
 	# Add the gravity.
+	
 	if not is_on_floor():
 		velocity.y += gravity * delta
 		
@@ -17,3 +18,12 @@ func _physics_process(delta):
 		velocity.y = JUMP_VELOCITY
 
 	move_and_slide()
+
+
+func _on_cooltimer_timeout():
+	if(flag == 1):
+		velocity.x = -SPEED
+		flag = -flag
+	else:
+		velocity.x = SPEED
+		flag = -flag
