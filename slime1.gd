f37b6a667998b0e21499618534862a3468253401
wheatgreaser <wheatgreaser3000@gmail.com>
2025-01-28 09:54:54 +0530

    hmm

--- slime1.gd
+++ slime1.gd
@@ -2,8 +2,9 @@ extends CharacterBody2D
 
 class_name Slime
 const SPEED = 300.0
-const JUMP_VELOCITY = -1000.0
-var health = 100
+var JUMP_VELOCITY = -1000.0
+var health = 50
+
 
 @onready var healthbar = $Healthbar
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -22,6 +23,17 @@ func _physics_process(delta):
 	if is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	if health <= 0:
+		var copy1 = duplicate()
+		var copy2 = duplicate()
+		copy1.scale = Vector2(2, 2)
+		copy2.scale = Vector2(2, 2)
+		copy1.JUMP_VELOCITY = -500
+		copy2.JUMP_VELOCITY = -500
+		copy2.global_position = global_position + Vector2(10, 10)
+		copy1.global_position = copy2.global_position + Vector2(70 , -80)
+		$"../slimeers".add_child(copy1)
+		$"../slimeers".add_child(copy2)
+		
 		delete_self()
 	move_and_slide()
 
